8d9dd2e148cae4108d4cbc4eaa25478e1694e7fa
pezholio <pezholio@gmail.com>
2018-05-31 21:59:35 +0100

    Mount webhook engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ContentfulRails::Engine => '/contentful'
   
   resources :blog_posts, path: 'blog', only: [:index, :show]
 end
