8af8b6846f90b7583ae2fe6dd8ce4bd656bd89a7
pezholio <pezholio@gmail.com>
2018-06-06 16:18:50 +0100

    Add basic homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, only: :expire_cache
   
+  def index; end
+  
   def expire_cache
     ExpireCache.new(self).perform
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'application#index'
+  
   resources :blog_posts, path: 'blog', only: %i[index show]
   resources :people, only: %i[show]
 
