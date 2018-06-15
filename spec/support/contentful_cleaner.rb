57118fedfe865d36377579c1b8c2ba597c31279b
pezholio <pezholio@gmail.com>
2018-06-15 11:43:24 +0100

    Use singleton for objects_to_delete

--- spec/support/contentful_cleaner.rb
+++ spec/support/contentful_cleaner.rb
@@ -1,12 +1,24 @@
 require 'contentful'
+require 'singleton'
+
+module Contentful
+  class Cleaner
+    include Singleton
+    
+    attr_accessor :objects_to_delete
+    
+    def initialize
+      @objects_to_delete = []
+    end
+  end
+end
 
 module ContentfulModel
   class Base < ::Contentful::Entry
-    OBJECTS_TO_DELETE = []
     
     def self.create(values = {}, publish = false)
       obj = super(values, publish)
-      OBJECTS_TO_DELETE << obj
+      Contentful::Cleaner.instance.objects_to_delete << obj
       obj
     end
     
