07d2913af8616d3b81684c854557ab1812030f0d
Chris Evans <evanschris@users.noreply.github.com>
2018-06-22 17:03:34 +0100

    strcutre blog pages

--- app/decorators/blog_post_decorator.rb
+++ app/decorators/blog_post_decorator.rb
@@ -1,25 +1,25 @@
 class BlogPostDecorator < Draper::Decorator
   delegate_all
-  
+
   def decorate_title
     h.content_tag :h1, object.title
   end
-  
+
   def decorate_subheading
     h.content_tag :p, h.raw(object.subheading), class: 'subheading'
   end
-  
+
   def decorate_date
     date = object.try(:date) || object.created_at
     h.content_tag :p, date.strftime('%e %B %Y'), class: 'date'
   end
-  
+
   def decorate_author
     h.content_tag :p, '', class: 'author' do
       "By #{h.link_to(object.author.name, object.author.path)}".html_safe
     end
   end
-  
+
   def decorate_body
     h.parse_markdown(object.body)
   end
