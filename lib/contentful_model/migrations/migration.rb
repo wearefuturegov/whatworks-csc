bcb607f93beccac50b887f61bbb48cb9ced6b1c8
pezholio <pezholio@gmail.com>
2018-06-25 16:00:46 +0100

    Fix migration runner

--- lib/contentful_model/migrations/migration.rb
+++ lib/contentful_model/migrations/migration.rb
@@ -4,7 +4,7 @@ module ContentfulModel
       
       def create_content_type(name, fields = {}, &block)
         result = ContentfulModel::Migrations::ContentTypeFactory.create(name, fields, &block)
-        if result.instance_variable_get('@management_content_type').class == Contentful::Management::Conflict
+        if [Contentful::Management::Conflict, Contentful::Management::BadRequest].include? result.instance_variable_get('@management_content_type').class
           puts "Content type #{result.name} already exists"
         else
           puts "Creating content type #{result.name}"
