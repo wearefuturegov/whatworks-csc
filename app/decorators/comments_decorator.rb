f1ee749b96a61434ff47dcd23ec02a209a6feb4d
Chris Evans <evanschris@users.noreply.github.com>
2018-06-26 16:16:54 +0100

    fix org decorator in comments

--- app/decorators/comments_decorator.rb
+++ app/decorators/comments_decorator.rb
@@ -1,18 +1,18 @@
 class CommentsDecorator < ApplicationDecorator
   delegate_all
-  
+
   def decorate_name
     standard_wrapper('Name', object.name)
   end
-  
+
   def decorate_organisation
-    standard_wrapper('Organisation', object.organisation)
+    standard_wrapper('Organisation', object.try(:organisation))
   end
-  
+
   def decorate_comments
     h.content_tag(:div, '', class: 'comment') do
       h.content_tag(:blockquote, h.parse_markdown(object.comment))
     end
   end
-  
+
 end
