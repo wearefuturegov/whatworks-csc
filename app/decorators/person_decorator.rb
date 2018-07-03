13755dcd9276f71e6e47015073adfb779168fba8
pezholio <pezholio@gmail.com>
2018-07-03 12:14:59 +0100

    Only show bio if it exists
    Also DRY up template logic

--- app/decorators/person_decorator.rb
+++ app/decorators/person_decorator.rb
@@ -5,4 +5,22 @@ class PersonDecorator < ApplicationDecorator
     return unless defined?(object.photo)
     h.image_tag object.photo.resize(200, 200).thumbnail_focused_on('face').load
   end
+  
+  def decorate_name
+    h.content_tag('h1', object.name)
+  end
+  
+  def decorate_role
+    h.content_tag('h4', object.role) if defined?(object.role)
+  end
+  
+  def decorate_bio
+    if defined?(object.bio)
+      h.parse_markdown(object.bio)
+    else
+      h.content_tag('p', '') do
+        h.content_tag('i', 'Biography coming soon')
+      end
+    end
+  end
 end
