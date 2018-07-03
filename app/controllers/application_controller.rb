3b18b3680cfb9bca3c292f88a805401b7b00b2ef
pezholio <pezholio@gmail.com>
2018-07-03 10:13:03 +0100

    Order homepage blog posts by date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, only: :expire_cache
   
   def index
-    @blog_posts = BlogPost.all.order(:created_at).limit(3).load
+    @blog_posts = BlogPost.all.order(date: :desc).limit(3).load
   end
   
   def expire_cache
