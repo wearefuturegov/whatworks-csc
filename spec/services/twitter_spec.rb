5c4d8b87f6b841c94b96df21f5162c6179fc168b
pezholio <pezholio@gmail.com>
2018-07-03 13:59:11 +0100

    Fix tests

--- spec/services/twitter_spec.rb
+++ spec/services/twitter_spec.rb
@@ -5,7 +5,7 @@ describe WhatWorksCentre::Twitter, vcr: true do
   let(:subject) { WhatWorksCentre::Twitter.instance }
   
   it 'gets the latest tweet' do
-    expect(subject.latest_tweet).to eq('Yes our very own @ThatGirlVim will be live on @DigiLeaders at 5pm today: Meet the young disruptors… https://t.co/YLiTQRb0hi')
+    expect(subject.latest_tweet.full_text).to eq('Yes our very own @ThatGirlVim will be live on @DigiLeaders at 5pm today: Meet the young disruptors… https://t.co/YLiTQRb0hi')
   end
   
 end
