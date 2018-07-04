43a03911c90ae9f94ad0348752e35526657bf650
pezholio <pezholio@gmail.com>
2018-07-04 09:57:00 +0100

    Tweak embed

--- app/assets/javascripts/embed_vlogs.js
+++ app/assets/javascripts/embed_vlogs.js
@@ -4,11 +4,23 @@ $.fn.embedVlogs = function() {
   
   var $links = $(this).find('a[href^="https:\/\/www.youtube"]')
   
+  var getId = function(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+  }
+  
   $.each($links, function() {
-    var link = this
-    $.getJSON('https://noembed.com/embed?url=' + link.href, function(data) {
-      $(link).replaceWith(data.html);
+    var id = getId(this.href)
+    var $iframe = $('<iframe>', {
+      src: 'https://www.youtube.com/embed/'+ id +'?feature=oembed',
+      width: '100%',
+      height: '56.25vw',
+      allowfullscreen: 'allowfullscreen',
+      frameborder: 0
     })
+    
+    $(this).replaceWith($iframe);
   })
 
 };
