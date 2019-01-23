ee1acc50fb8e0515403060d8fb2f8ffc4c80b301
Chris Evans <evanschris@users.noreply.github.com>
2019-01-23 16:39:09 +0000

    fix missing research reports

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,18 @@
 class PagesController < ApplicationController
   include ContentfulController
-  
-  prepend_before_action :fetch_research_reports, if: -> { params[:id] == 'research-topics' }
+
+  prepend_before_action :fetch_research_reports, if: -> { params[:id] == 'research' }
   prepend_before_action :fetch_prototypes, if: -> { params[:id] == 'co-designing-with-partners' }
 
   def show; end
-  
+
   private
-  
+
   def fetch_section
     @section = Section.load_children(10).find_by(slug: params[:section_id]).load.first
     raise(ActionController::RoutingError, 'Not Found') if @section.nil?
   end
-  
+
   def load_content
     check_preview
     fetch_section
@@ -20,11 +20,11 @@ class PagesController < ApplicationController
     raise(ActionController::RoutingError, 'Not Found') if content.nil?
     @page = content.decorate
   end
-  
+
   def fetch_research_reports
     @research_reports = ResearchReport.all.load
   end
-  
+
   def fetch_prototypes
     @prototypes = Prototype.all.load
   end
